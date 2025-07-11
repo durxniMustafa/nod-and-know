539f631b87b5aff915ad35071a7f183063c0e4be
Mustafa <M.Durani@campus.lmu.com>
2025-07-11 17:38:49 +0200

    FixedIP

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const isPrivateIP = (ip: string): boolean =>
+  /^(10\.|192\.168\.|172\.(1[6-9]|2[0-9]|3[01])\.)/.test(ip);
